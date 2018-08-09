b0a5abd24497f82386dd81e9ebaa1aba82b402b6
7jpsan <7jpsan@gmail.com>
2018-08-09 16:16:36 +0100

    Day 8 - Stars

---
+++ 2017/millisecond-9/stars.ts
@@ -0,0 +1,71 @@
+import { readFileSync } from 'fs';
+
+const input = readFileSync('./input', 'utf8');
+
+type Process = {
+  input: string, 
+  total: number,
+  level: number,
+  garbage: number
+};
+
+type PTree = {
+  value: 0,
+  subTrees: PTree[];
+};
+
+const processMap = input.toString().trim().split("\n").map(entryToProcess);
+processMap.forEach(scoreStream);
+
+function scoreStream(p: Process): void{
+
+  const stream = [...p.input];
+
+  let inGarbage = false;
+  let ignoreNext = false;
+
+  stream.forEach(x => {
+    
+    if(inGarbage){
+      if(!ignoreNext){
+        if(x == '!'){
+          ignoreNext = true;
+        }else if(x == '>'){
+          inGarbage = false;
+        }else{
+          p.garbage++;
+        }
+      }else{
+        ignoreNext = false;
+      }
+    }else{
+      switch(x){
+        case '<':
+          inGarbage = true;
+          break;
+        case '{':
+          p.level++;
+          break;
+        case '}':
+          p.total = p.total + p.level
+          p.level--;
+          break;
+      }
+    }
+
+  });
+
+  console.log(`
+    Star 1 - ${p.total}
+    Star 2 - ${p.garbage}
+  `);
+}
+
+function entryToProcess(entry: string): Process{
+  return {
+    level: 0,
+    input: entry,
+    garbage: 0,
+    total: 0
+  } as Process;
+};

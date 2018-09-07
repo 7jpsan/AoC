f1c9bbb1afb34eb1c2728da4b20e1376784ebf3f
7jpsan <7jpsan@gmail.com>
2018-09-07 17:49:31 +0100

    Day 12 - Stars

---
+++ 2017/millisecond-12/stars.ts
@@ -0,0 +1,48 @@
+import { readFileSync } from 'fs';
+import { includes, difference, union } from 'lodash';
+
+
+type PipeMap = {
+  [end: number]: number[]
+}
+
+const pipeMap: PipeMap = {};
+
+// Start processing
+const input = readFileSync(__dirname+'/input', 'utf8');
+
+const parsedInput = input.toString().trim().split("\n").forEach(x => {
+  const p = x.split('<->');
+  pipeMap[+p[0]] = p[1].replace(/\s+/g, '').split(',').map(y => +y);
+});
+
+
+const all: number[] = Object.keys(pipeMap).map(x => +x);
+const connected: number[] = [];
+const visited: number[] = [];
+const pending: number[] = [];
+
+let groups = 0;
+
+while(difference(all, visited).length > 0){
+
+  pending.push(difference(all, visited)[0]);
+
+  while(pending.length > 0){
+  
+    const v = pending.pop()!;
+  
+    if(!includes(visited, v)){
+      visited.push(v);
+      connected.push(v);
+      pending.push(...pipeMap[v]);
+      // pending.push(...difference(pipeMap[v], union(visited, connected))); // 231 iterations less, 20ms more (star 1)...
+    }
+  }
+  groups++;
+}
+
+console.log(`
+  Star 1 - ${connected.length}
+  Star 2 - ${groups}
+`);
